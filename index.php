f530ddc29120e24bfcb150f74089d2c7b4290d7a
Leo <leo.vornberger@gmx.de>
2020-04-13 20:19:41 +0200

    Kleiner Failsafe-Mechanismus im index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-    if (isset($_COOKIE["spielerName"])) {
+    $fp = fopen("spielstatus.txt", r);
+    $status = fgets($fp, filesize("spielstatus.txt")+1);
+    fclose($fp);
+    
+    if (isset($_COOKIE["spielerName"]) && $status == "spielLaeuft") {
         echo "<html><head><meta http-equiv=\"refresh\" content=\"0; URL=http://www.leo.vornberger.net/munchkin/munchkin.php\"></head><body></body></html>";
     }
     else {
