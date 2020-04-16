d4cb9ed7f261a2d7898710d46e328134da7277e4
Leo <leo.vornberger@gmx.de>
2020-04-16 20:57:58 +0200

    Verhindern gleichzeiter Schreibvorgänge bei Spielstandsänderung

--- spielstandManager.php
+++ spielstandManager.php
@@ -2,38 +2,36 @@
     if (isset($_COOKIE["spielerId"])) {
         $spielerId = $_COOKIE["spielerId"];
         if (isset($_POST["deltaStufe"])) {
-            $fp = fopen("spieler.txt", r);
-            $data = fgets($fp, filesize("spieler.txt")+1);
-            fclose($fp);
-            
-            $spieler = explode("/", $data);
-            $nameDesSpielersMitDerNeuenStufe = explode(";", $spieler[$spielerId])[0];
-            
-            $dataNeu = "";
-            for ($i = 0; $i < count($spieler); $i++) {
-                if ($i == $spielerId) {
-                    $alteStufe = explode(";", $spieler[$spielerId])[1];
-                    $dataNeu = $dataNeu . $nameDesSpielersMitDerNeuenStufe . ";" . ($alteStufe + $_POST["deltaStufe"]);
+            $fp = fopen("spieler.txt", "r+");
+            if (flock($fp, LOCK_EX)) {
+                $data = fgets($fp, 4096);
+                $spieler = explode("/", $data);
+                $nameDesSpielersMitDerNeuenStufe = explode(";", $spieler[$spielerId])[0];
+                
+                $dataNeu = "";
+                $neueStufe = "";
+                for ($i = 0; $i < count($spieler); $i++) {
+                    if ($i == $spielerId) {
+                        $alteStufe = explode(";", $spieler[$spielerId])[1];
+                        $neueStufe = ($alteStufe + $_POST["deltaStufe"]) . "";
+                        $dataNeu = $dataNeu . $nameDesSpielersMitDerNeuenStufe . ";" . $neueStufe;
+                    }
+                    else {
+                        $dataNeu = $dataNeu . $spieler[$i];
+                    }
+                    if ($i != count($spieler) - 1) {
+                        $dataNeu = $dataNeu . "/";
+                    }
                 }
-                else {
-                    $dataNeu = $dataNeu . $spieler[$i];
-                }
-                if ($i != count($spieler) - 1) {
-                    $dataNeu = $dataNeu . "/";
-                }
+                
+                ftruncate($fp, 0);
+                fputs($fp, $dataNeu);
+                fclose($fp);
+                echo "Spieler " . $spielerId . " (" . $nameDesSpielersMitDerNeuenStufe . ") ist jetzt auf Stufe " . $neueStufe . ".";
             }
-            
-            $fp = fopen("spieler.txt", w);
-            fputs($fp, $dataNeu);
-            fclose($fp);
-            
-            // Antwort:
-            $fp = fopen("spieler.txt", r);
-            $data = fgets($fp, filesize("spieler.txt")+2);
-            fclose($fp);
-            
-            $neuesteStufe = explode(";", explode("/", $data)[$spielerId])[1];
-            echo "Spieler " . $spielerId . " (" . $nameDesSpielersMitDerNeuenStufe . ") ist jetzt auf Stufe " . $neuesteStufe . ". Data = '" . $data . "'";
+            else {
+                echo "Datei 'spieler.txt' kann nicht geöffnet werden";
+            }
         }
     }
     ?>
